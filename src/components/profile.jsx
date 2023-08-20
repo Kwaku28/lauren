4e422aee9336d1189c585acc70560a3615eb64d3
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-20 17:07:24 +0000

    Add function to display the profile

---
+++ src/components/profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Welcome.</h1>
+      <p>
+        As a full stack developer, my skill set covers HTML, CSS, JavaScript,
+        React, Redux, and Ruby on Rails. This expertise enables me to create
+        visually appealing and responsive web interfaces while also implementing
+        robust back-end functionalities.
+        <br />
+        I excel at delivering comprehensive web solutions that cater to
+        both user experience and business requirements. I am also passionate
+        about cutting-edge, pixel-perfect, beatiful interfaces and intuitively
+        implemented UX.
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
