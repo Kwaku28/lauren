93a9bd10858b69f9218e63bb1b48ef830f7d811a
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-21 00:15:55 +0000

    Add logo to page

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import lauren from "/lauren.png";
 
 const Navigation = () => {
   const [showProfile, setShowProfile] = useState(true);
@@ -9,20 +10,41 @@ const Navigation = () => {
 
   return (
     <nav>
-      <h1 onClick={toggleProfile} style={{ cursor: 'pointer' }}>Lawrence</h1>
+      <div>
+        <img
+          src={lauren}
+          alt="Logo"
+          onClick={toggleProfile}
+          style={{ cursor: "pointer", width: "10rem" }}
+        />
+      </div>
       <ul>
         <li>
-          <a href="mailto:rekenburg777@gmail.com" target="_blank" rel="noreferrer">Email</a>
+          <a
+            href="mailto:rekenburg777@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Email
+          </a>
         </li>
         <li>
-          <a href="https://github.com/Kwaku28" target="_blank" rel="noreferrer">Github</a>
+          <a href="https://github.com/Kwaku28" target="_blank" rel="noreferrer">
+            Github
+          </a>
         </li>
         <li>
-          <a href="https://linkedin.com/in/lawrence-amoafo-appoh" target="_blank" rel="noreferrer">Linkedin</a>
+          <a
+            href="https://linkedin.com/in/lawrence-amoafo-appoh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedin
+          </a>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navigation;
