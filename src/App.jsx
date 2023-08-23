415f5fd431a56d68e7c02e736411a187250e872f
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-23 18:18:25 +0000

    Implement function to toggle between projects and profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,75 @@
-import { useState } from 'react'
-import Navigation from './components/navigation'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Navigation from "./components/navigation";
+import Profile from "./components/profile";
+import Project from "./components/projects";
+import projectData from "./components/data/projectData";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(null);
+  const [currentContent, setCurrentContent] = useState("profile");
+
+  // Function to handle toggling between Profile and projects
+  const toggleContent = () => {
+    if (currentContent === "profile") {
+      if (currentProjectIndex !== null) {
+        setCurrentContent("project");
+      }
+    } else {
+      setCurrentContent("profile");
+    }
+  };
+
+  // Function to handle toggling between project titles and details
+  const toggleProject = (index) => {
+    if (currentProjectIndex === index) {
+      toggleContent();
+    } else {
+      setCurrentProjectIndex(index);
+      setCurrentContent("project");
+    }
+  };
+
+  const toggleToProfile = () => {
+    setCurrentContent('profile');
+  };
 
   return (
     <>
-      <Navigation />
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navigation toggleProfile={toggleToProfile} />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            {currentContent === "profile" ? (
+              <Profile />
+            ) : (
+              currentProjectIndex !== null && (
+                <Project project={projectData[currentProjectIndex]} />
+              )
+            )}
+          </div>
+          <div className="col-md-4">
+            <h2>Projects</h2>
+            <ul>
+              {projectData.map((project, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => toggleProject(index)}
+                    className={`btn btn-link ${
+                      index === currentProjectIndex ? "active" : ""
+                    }`}
+                  >
+                    {project.title}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
