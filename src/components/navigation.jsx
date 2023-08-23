56a63f1200a00638fc6c9d55dcf2470d0fd9dc4d
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-23 18:16:47 +0000

    Add a prop to `Navigation` function to help toggle profile section

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,50 +1,60 @@
-import { useState } from "react";
+import PropTypes from "prop-types";
 import lauren from "/lauren.png";
+import Nav from "react-bootstrap/Nav";
+import { SiGmail, SiGithub, SiLinkedin } from "react-icons/si";
 
-const Navigation = () => {
-  const [showProfile, setShowProfile] = useState(true);
-
-  const toggleProfile = () => {
-    setShowProfile(!showProfile);
-  };
+const Navigation = ({ toggleProfile }) => {
 
   return (
-    <nav>
-      <div>
-        <img
-          src={lauren}
-          alt="Logo"
-          onClick={toggleProfile}
-          style={{ cursor: "pointer", width: "10rem" }}
-        />
-      </div>
-      <ul>
-        <li>
-          <a
-            href="mailto:rekenburg777@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Email
+    <>
+      <Nav className="navbar navbar-expand-lg">
+        <div className="navigaton container-fluid d-flex justify-content-between px-5">
+          <a className="nav-brand">
+            <img
+              src={lauren}
+              alt="Logo"
+              onClick={toggleProfile}
+              style={{ cursor: "pointer", width: "10rem" }}
+              className="logo"
+            />
           </a>
-        </li>
-        <li>
-          <a href="https://github.com/Kwaku28" target="_blank" rel="noreferrer">
-            Github
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://linkedin.com/in/lawrence-amoafo-appoh"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Linkedin
-          </a>
-        </li>
-      </ul>
-    </nav>
+          <ul className="navbar-nav me-5 gap-5">
+            <li className="nav-item">
+              <a
+                href="mailto:rekenburg777@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiGmail />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://github.com/Kwaku28"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiGithub />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://linkedin.com/in/lawrence-amoafo-appoh"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiLinkedin />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Nav>
+    </>
   );
+};
+
+Navigation.propTypes = {
+  toggleProfile: PropTypes.func.isRequired,
 };
 
 export default Navigation;
