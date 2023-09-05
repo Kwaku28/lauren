4856cc6d2f83eab7b5be5e4f197414d0dc3805a0
Lawrence Amoafo <rekenburg777@gmail.com>
2023-09-05 09:01:33 +0000

    Add classnames to elements

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,17 +1,18 @@
 import PropTypes from "prop-types";
+import '../styles/projects.css'
 
 const Project = ({ project }) => {
   return (
-    <div className="">
-      <h5 className="">{project.title}</h5>
-      <p className="">{project.description}</p>
+    <div className="project-container">
+      <h2>{project.title}</h2>
+      <p className="project-description">{project.description}</p>
       <div className="d-flex justify-content-between align-items-center">
         <div className="btn-group gap-4">
           <a
             href={project.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-sm btn-dark"
+            className="project-btn"
           >
             View
           </a>
@@ -19,7 +20,7 @@ const Project = ({ project }) => {
             href={project.github}
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-sm btn-dark"
+            className="project-btn"
           >
             Code
           </a>
