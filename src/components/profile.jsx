dc509e82f08cbf3c63cd28ccbbbcda943ecc198b
Lawrence Amoafo <rekenburg777@gmail.com>
2023-09-05 09:02:17 +0000

    Create a SVG-Text animation

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,16 +1,20 @@
-import '../styles/profile.css';
+import "../styles/profile.css";
 
 const Profile = () => {
   return (
-    <div>
-      <h1 className="heading">Welcome.</h1>
+    <div className="profile-container">
+      <svg className="heading">
+        <text x="0%" y="50%" dy=".55em">
+          Welcome.
+        </text>
+      </svg>
       <p className="profile">
         As a full stack developer, my skill set covers HTML, CSS, JavaScript,
         React, Redux, and Ruby on Rails. This expertise enables me to create
         visually appealing and responsive web interfaces while also implementing
         robust back-end functionalities.
-        <br /><br />
-        I excel at delivering comprehensive web solutions that cater to
+        <br />
+        <br />I excel at delivering comprehensive web solutions that cater to
         both user experience and business requirements. I am also passionate
         about cutting-edge, pixel-perfect, beautiful interfaces and intuitively
         implemented UX.
