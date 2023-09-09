0489be5033959a208086763c4864b2152bcd4206
Lawrence Amoafo <rekenburg777@gmail.com>
2023-09-09 19:55:59 +0000

    Update app with projects

--- src/components/data/projectData.jsx
+++ src/components/data/projectData.jsx
@@ -1,30 +1,27 @@
 const projectData = [
   {
     id: 1,
-    title: 'Vite + React',
-    description: 'A Vite + React starter template.',
-    image: 'https://raw.githubusercontent.com/vitejs/vite/main/assets/vite.png',
-    url: 'https://vitejs.dev',
-    github: 'https://github.com',
-    date: '2021-01-01',
+    title: "Grow Rich",
+    description:
+      "Grow Rich is a mobile web application where you can manage your budget: you have a list of expenses associated with a budget, so that you can see how much money you spent and on what. You have the flexibility to add budget and expenses as well.",
+    url: "https://grow-rich.onrender.com/",
+    github: "https://github.com/Kwaku28/Grow-Rich",
   },
   {
     id: 2,
-    title: 'Vite',
-    description: 'A Vite + React starter template.',
-    image: 'https://raw.githubusercontent.com/vitejs/vite/main/assets/vite.png',
-    url: 'https://vitejs.dev',
-    github: 'https://github.com',
-    date: '2021-01-01',
+    title: "Bookstore",
+    description:
+      "This is an open-source app that categorizes books based on the genre. The app gives the option to add a new book, keep track of your reading progress.",
+    url: "https://bookstore-7igy.onrender.com/",
+    github: "https://github.com/Kwaku28/Bookstore",
   },
   {
     id: 3,
-    title: 'Vite + React',
-    description: 'A Vite + React starter template.',
-    image: 'https://raw.githubusercontent.com/vitejs/vite/main/assets/vite.png',
-    url: 'https://vitejs.dev',
-    github: 'https://github.com',
-    date: '2021-01-01',
+    title: "Space travellers hub",
+    description:
+      "This is a web application for a company that provides commercial and scientific space travel services. The application allow users to book rockets and join selected space missions. It also give the option to cancel reservations and leave mission anytime. The MyProfile section gives and overview of the your booked rockets and joined missions.",
+    url: "https://space-travelers-hub-sum7.onrender.com/",
+    github: "https://github.com/Kwaku28/Space-Travelers-Hub",
   },
 ];
 
